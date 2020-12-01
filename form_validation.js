cffcacda6ba647d28fe442ba09b5f7d07c2cb00e
PrabhavJain10 <67673958+PrabhavJain10@users.noreply.github.com>
2020-12-01 17:31:23 +0800

    form validation updated

--- form_validation.js
+++ form_validation.js
@@ -1,4 +1,10 @@
 var frmvalidator  = new Validator("contact_form");
-frmvalidator.addValidation("name","req","Please provide your name");
-frmvalidator.addValidation("email","req","Please provide your email");
-frmvalidator.addValidation("email","email","Please enter a valid email address");
+frmvalidator.addValidation("name","req", "Please provide your name");
+frmvalidator.addValidation("email","req", "Please provide your email");
+frmvalidator.addValidation("email","email", "Please enter a valid email address");
+frmvalidator.addValidation("phone","req", "Please provide your phone number");
+frmvalidator.addValidation("phone","maxlen=10", "Please provide a valid phone number");
+frmvalidator.addValidation("phone","numeric", "Please provide a valid phone number");
+frmvalidator.addValidation("contact","selone", "Please select a preferred contact method");
+frmvalidator.addValidation("message","req", "Please provide a message");
+frmvalidator.addValidation("message","minlen=10", "Please provide a meaningful message");
